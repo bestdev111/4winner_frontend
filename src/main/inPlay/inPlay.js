a00b783db2dc8045973122a7e40f87627e0e6692
jack <jack.lin9316@gmail.com>
2022-10-19 10:53:55 +0300

    remove left menu

--- src/main/inPlay/inPlay.js
+++ src/main/inPlay/inPlay.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './inPlay.css'
-import { RightPanel, LeftMenu, Divider } from 'components';
+import { RightPanel, LeftMenu, Divider, Table } from 'components';
 const InPlay = () => (
     <div className='container-fluid d-flex flex-column ptt'>
         <div className='row'>
             <div className='left px-2 float-left'>
-                <LeftMenu />
-            </div>
-            <div className='center px-2 float-left'>
                 <Divider title='Highlights' />
+                <Table
+                    title="Highlights"
+                />
                 <Divider title='Top Odds' />
+                <Table
+                    title="Top Odds"
+                />
             </div>
             <div className='right px-2 float-left'>
                 <RightPanel />
