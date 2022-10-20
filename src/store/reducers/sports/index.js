8df2c6870c0d4c181fc5b7e40ee7eb84646f8bad
jack <=>
2022-10-20 22:33:52 +0300

    fix error in using redux

--- src/store/reducers/sports/index.js
+++ src/store/reducers/sports/index.js
@@ -1,15 +1,9 @@
 import {combineReducers} from 'redux';
-import navigation from './navigation.reducer';
 import navbar from './navbar.reducer';
-import message from './message.reducer';
-import dialog from './dialog.reducer';
 import routes from './routes.reducer';
 
 const fuseReducers = combineReducers({
-    navigation,
     navbar,
-    message,
-    dialog,
     routes
 });
 
