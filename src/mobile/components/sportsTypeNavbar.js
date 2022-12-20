59aaf915667baf2b7cfc6cc7afcd9ee024a32bd0
jack <=>
2022-12-21 00:07:07 +0300

    ordering sportsType

--- src/mobile/components/sportsTypeNavbar.js
+++ src/mobile/components/sportsTypeNavbar.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import './styles/mobileNavbar.css'
 
 function SportsTypeNavbar(props) {
     const [sportActiveVal, setSportActiveVal] = useState(1);
+    const [orderedSportsList, setOrderedSportsList] = useState();
     const SportTypeList = useSelector(state => state.mobileSportsReducers.getTypeList);
     const get_AllMatches = useSelector(state => state.mobileSportsReducers.getAllMatches);
     const sportActiveChange = (index, typeName) => {
@@ -12,9 +13,14 @@ function SportsTypeNavbar(props) {
     }
     let availableSportTypes = '';
     if (get_AllMatches.data) { availableSportTypes = get_AllMatches.data.availableSportTypes }
+    useEffect(()=> {
+        if(SportTypeList){
+            SportTypeList.sort((a, b) => a.order - b.order);
+        }
+    }, [SportTypeList])
     return (
         <div className='d-flex sports-type'>
-            {availableSportTypes && availableSportTypes.map((availableSportType, index) =>
+            {/* {availableSportTypes && availableSportTypes.map((availableSportType, index) =>
                 <div 
                     className={availableSportType === sportActiveVal ? 'item item-active' : 'item'} 
                     key={index} 
@@ -23,6 +29,18 @@ function SportsTypeNavbar(props) {
                     <img src={SportTypeList[availableSportType - 1] ? SportTypeList[availableSportType - 1].m_icon : ''} alt='' />
                     <p>{SportTypeList[availableSportType - 1] ? SportTypeList[availableSportType - 1].name : ''}</p>
                 </div>
+            )} */}
+            {SportTypeList && SportTypeList.map((item, index) =>
+                availableSportTypes.includes(item.sportTypeId) ?
+                    <div
+                        className={item.sportTypeId === sportActiveVal ? 'item item-active' : 'item'}
+                        key={index}
+                        onClick={() => sportActiveChange(item.sportTypeId, item.name)}
+                    >
+                        <img src={item.m_icon} alt='' />
+                        <p>{item.name}</p>
+                    </div>
+                : null
             )}
         </div>
     );
--- src/utils/dataUtils.js
+++ src/utils/dataUtils.js
@@ -7,17 +7,17 @@ const SportTypeList = [
         order: 1
     },
     {
+        sportTypeId: 5,
+        name: 'Tennis',
+        icon: '../assets/images/sports/tennis.png',
+        m_icon: '../assets/images/micons/tennis.svg',
+        order: 2
+    },
+    {
         sportTypeId: 2,
         name: 'Basketball',
         icon: '../assets/images/sports/basketball.png',
         m_icon: '../assets/images/micons/basketball.svg',
-        order: 1
-    },
-    {
-        sportTypeId: 3,
-        name: 'Baseball',
-        icon: '../assets/images/sports/baseball.png',
-        m_icon: '../assets/images/micons/baseball.svg',
         order: 3
     },
     {
@@ -28,147 +28,147 @@ const SportTypeList = [
         order: 4
     },
     {
-        sportTypeId: 5,
-        name: 'Tennis',
-        icon: '../assets/images/sports/tennis.png',
-        m_icon: '../assets/images/micons/tennis.svg',
-        order: 2
-    },
-    {
-        sportTypeId: 6,
-        name: 'Handball',
-        icon: '../assets/images/sports/handball.png',
-        m_icon: '../assets/images/micons/handball.svg',
-        order: 6
-    },
-    {
-        sportTypeId: 7,
-        name: 'xxx',
-        order: 1
-    },
-    {
-        sportTypeId: 8,
-        name: 'xxx',
-        order: 1
-    },
-    {
-        sportTypeId: 9,
-        name: 'xxx',
-        order: 1
-    },
-    {
-        sportTypeId: 10,
-        name: 'Boxing',
-        icon: '../assets/images/sports/boxing.png',
-        m_icon: '../assets/images/micons/boxing.svg',
-        order: 9
-    },
-    {
-        sportTypeId: 11,
-        name: 'xxx',
-        order: 1
-    },
-    {
-        sportTypeId: 12,
-        name: 'Rugby',
-        icon: '../assets/images/sports/rugby.png',
-        m_icon: '../assets/images/micons/rugby.svg',
-        order: 11
-    },
-    {
-        sportTypeId: 13,
-        name: 'xxx',
-        order: 1
-    },
-    {
-        sportTypeId: 14,
-        name: 'xxx',
-        order: 1
-    },
-    {
-        sportTypeId: 15,
-        name: 'xxx',
-        order: 1
-    },
-    {
-        sportTypeId: 16,
-        name: 'American Football',
-        icon: '../assets/images/sports/american_football.png',
-        m_icon: '../assets/images/micons/american_football.svg',
-        order: 12
-    },
-    {
-        sportTypeId: 17,
-        name: 'xxx',
-        order: 1
-    },
-    {
-        sportTypeId: 18,
-        name: 'xxx',
-        order: 1
-    },
-    {
-        sportTypeId: 19,
-        name: 'Snooker',
-        icon: '../assets/images/sports/snooker.png',
-        m_icon: '../assets/images/micons/snooker.svg',
-        order: 10
-    },
-    {
-        sportTypeId: 20,
-        name: 'xxx',
-        order: 1
-    },
-    {
-        sportTypeId: 21,
-        name: 'xxx',
-        order: 1
-    },
-    {
-        sportTypeId: 22,
-        name: 'Darts',
-        icon: '../assets/images/sports/darts.png',
-        m_icon: '../assets/images/micons/darts.svg',
-        order: 8
+        sportTypeId: 3,
+        name: 'Baseball',
+        icon: '../assets/images/sports/baseball.png',
+        m_icon: '../assets/images/micons/baseball.svg',
+        order: 5
     },
     {
         sportTypeId: 23,
         name: 'Volleyball',
         icon: '../assets/images/sports/volleyball.png',
         m_icon: '../assets/images/micons/volleyball.svg',
-        order: 5
+        order: 6
     },
     {
-        sportTypeId: 24,
-        name: 'xxx',
-        order: 1
-    },
-    {
-        sportTypeId: 25,
-        name: 'xxx',
-        order: 1
-    },
-    {
-        sportTypeId: 26,
-        name: 'xxx',
-        order: 1
-    },
-    {
-        sportTypeId: 27,
-        name: 'xxx',
-        order: 1
-    },
-    {
-        sportTypeId: 28,
-        name: 'xxx',
-        order: 1
+        sportTypeId: 6,
+        name: 'Handball',
+        icon: '../assets/images/sports/handball.png',
+        m_icon: '../assets/images/micons/handball.svg',
+        order: 7
     },
     {
         sportTypeId: 29,
         name: 'Futsal',
         icon: '../assets/images/sports/futsal.png',
         m_icon: '../assets/images/micons/futsal.svg',
-        order: 7
+        order: 8
+    },
+    {
+        sportTypeId: 22,
+        name: 'Darts',
+        icon: '../assets/images/sports/darts.png',
+        m_icon: '../assets/images/micons/darts.svg',
+        order: 9
+    },
+    {
+        sportTypeId: 10,
+        name: 'Boxing',
+        icon: '../assets/images/sports/boxing.png',
+        m_icon: '../assets/images/micons/boxing.svg',
+        order: 10
+    },
+    {
+        sportTypeId: 19,
+        name: 'Snooker',
+        icon: '../assets/images/sports/snooker.png',
+        m_icon: '../assets/images/micons/snooker.svg',
+        order: 11
+    },
+    {
+        sportTypeId: 12,
+        name: 'Rugby',
+        icon: '../assets/images/sports/rugby.png',
+        m_icon: '../assets/images/micons/rugby.svg',
+        order: 12
+    },
+    {
+        sportTypeId: 16,
+        name: 'American Football',
+        icon: '../assets/images/sports/american_football.png',
+        m_icon: '../assets/images/micons/american_football.svg',
+        order: 13
+    },
+    {
+        sportTypeId: 7,
+        name: 'xxx',
+        order: 29
+    },
+    {
+        sportTypeId: 8,
+        name: 'xxx',
+        order: 29
+    },
+    {
+        sportTypeId: 9,
+        name: 'xxx',
+        order: 29
+    },
+    {
+        sportTypeId: 11,
+        name: 'xxx',
+        order: 29
+    },
+    {
+        sportTypeId: 13,
+        name: 'xxx',
+        order: 29
+    },
+    {
+        sportTypeId: 14,
+        name: 'xxx',
+        order: 29
+    },
+    {
+        sportTypeId: 15,
+        name: 'xxx',
+        order: 29
+    },
+    {
+        sportTypeId: 17,
+        name: 'xxx',
+        order: 29
+    },
+    {
+        sportTypeId: 18,
+        name: 'xxx',
+        order: 29
+    },
+    {
+        sportTypeId: 20,
+        name: 'xxx',
+        order: 29
+    },
+    {
+        sportTypeId: 21,
+        name: 'xxx',
+        order: 29
+    },
+    {
+        sportTypeId: 24,
+        name: 'xxx',
+        order: 29
+    },
+    {
+        sportTypeId: 25,
+        name: 'xxx',
+        order: 29
+    },
+    {
+        sportTypeId: 26,
+        name: 'xxx',
+        order: 29
+    },
+    {
+        sportTypeId: 27,
+        name: 'xxx',
+        order: 29
+    },
+    {
+        sportTypeId: 28,
+        name: 'xxx',
+        order: 29
     },
 ]
 export const tipTypesList = [
