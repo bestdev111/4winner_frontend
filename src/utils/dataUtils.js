993ffae257dc9db1182126bb489c410638ee05df
jack <=>
2022-12-20 21:55:06 +0300

    fix odd style of increase and decrease

--- src/utils/dataUtils.js
+++ src/utils/dataUtils.js
@@ -4,142 +4,171 @@ const SportTypeList = [
         name: 'Football',
         icon: '../assets/images/sports/football.png',
         m_icon: '../assets/images/micons/football.svg',
+        order: 1
     },
     {
         sportTypeId: 2,
         name: 'Basketball',
         icon: '../assets/images/sports/basketball.png',
         m_icon: '../assets/images/micons/basketball.svg',
+        order: 1
     },
     {
         sportTypeId: 3,
         name: 'Baseball',
         icon: '../assets/images/sports/baseball.png',
         m_icon: '../assets/images/micons/baseball.svg',
+        order: 3
     },
     {
         sportTypeId: 4,
         name: 'IceHockey',
         icon: '../assets/images/sports/icehockey.png',
         m_icon: '../assets/images/micons/icehockey.svg',
+        order: 4
     },
     {
         sportTypeId: 5,
         name: 'Tennis',
         icon: '../assets/images/sports/tennis.png',
         m_icon: '../assets/images/micons/tennis.svg',
+        order: 2
     },
     {
         sportTypeId: 6,
         name: 'Handball',
         icon: '../assets/images/sports/handball.png',
         m_icon: '../assets/images/micons/handball.svg',
+        order: 6
     },
     {
         sportTypeId: 7,
-        name: 'xxx'
+        name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 8,
-        name: 'xxx'
+        name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 9,
-        name: 'xxx'
+        name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 10,
         name: 'Boxing',
         icon: '../assets/images/sports/boxing.png',
         m_icon: '../assets/images/micons/boxing.svg',
+        order: 9
     },
     {
         sportTypeId: 11,
-        name: 'xxx'
+        name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 12,
         name: 'Rugby',
         icon: '../assets/images/sports/rugby.png',
         m_icon: '../assets/images/micons/rugby.svg',
+        order: 11
     },
     {
         sportTypeId: 13,
-        name: 'xxx'
+        name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 14,
-        name: 'xxx'
+        name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 15,
-        name: 'xxx'
+        name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 16,
         name: 'American Football',
         icon: '../assets/images/sports/american_football.png',
         m_icon: '../assets/images/micons/american_football.svg',
+        order: 12
     },
     {
         sportTypeId: 17,
-        name: 'xxx'
+        name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 18,
-        name: 'xxx'
+        name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 19,
         name: 'Snooker',
         icon: '../assets/images/sports/snooker.png',
         m_icon: '../assets/images/micons/snooker.svg',
+        order: 10
     },
     {
         sportTypeId: 20,
-        name: 'xxx'
+        name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 21,
-        name: 'xxx'
+        name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 22,
         name: 'Darts',
         icon: '../assets/images/sports/darts.png',
         m_icon: '../assets/images/micons/darts.svg',
+        order: 8
     },
     {
         sportTypeId: 23,
         name: 'Volleyball',
         icon: '../assets/images/sports/volleyball.png',
         m_icon: '../assets/images/micons/volleyball.svg',
+        order: 5
     },
     {
         sportTypeId: 24,
         name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 25,
-        name: 'xxx'
+        name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 26,
-        name: 'xxx'
+        name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 27,
-        name: 'xxx'
+        name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 28,
-        name: 'xxx'
+        name: 'xxx',
+        order: 1
     },
     {
         sportTypeId: 29,
         name: 'Futsal',
         icon: '../assets/images/sports/futsal.png',
         m_icon: '../assets/images/micons/futsal.svg',
+        order: 7
     },
 ]
 export const tipTypesList = [
