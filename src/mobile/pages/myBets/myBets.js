2082a7b59f044aa29d4ee7ebc6e3d0ffe54bd609
jack <=>
2022-12-28 13:34:44 +0300

    fix myBets page

--- src/mobile/pages/myBets/myBets.js
+++ src/mobile/pages/myBets/myBets.js
@@ -7,11 +7,16 @@ import './myBets.css'
 
 function MyBets() {
     const dispatch = useDispatch()
-    const [detailOpen, setDetailOpen] = useState(true);
+    const [detailOpen, setDetailOpen] = useState(null);
+    const [hold, setHold] = useState(null);
     const myBetData = useSelector(state => state.betReducers.myBetData);
     useEffect(() => {
         dispatch(getMyBet());
     }, [])
+    const funcHold = (index) => {
+        index === hold ? setHold(null) : setHold(index);
+    }
+    console.log('=>',myBetData);
     return (
         <>
             <MobileNavbar />
@@ -35,53 +40,182 @@ function MyBets() {
                             <option value='5'>Payed Out</option>
                         </select>
                     </div>
-                    {detailOpen ?
                         <div className='myaccount-table'>
-                            <div className='match-header'>
-                                <div className='col-3 text-center'>Type</div>
-                                <div className='col-4 text-center'>Date</div>
-                                <div className='col-2 text-center'>Stake</div>
-                                <div className='col-3 text-center'>Winnings</div>
-                            </div>
-                            <div className='match-body'>
-                                {myBetData !== null && myBetData.length > 0 ?
-                                    myBetData.map((item, index) =>
-                                        <div key={index} onClick={() => setDetailOpen(false)} className={item.state === 0 ? 'bet-result-wait' : item.state === 1 ? 'bet-result-win' : 'bet-result-lose'}>
-                                            <div className='col-3 text-center p-2'>{item.betSystem}</div>
-                                            <div className='col-4 text-center p-2'>{GetTime(item.date)}</div>
-                                            <div className='col-2 text-center p-2'>{item.initialStake.toFixed(2)}</div>
-                                            <div className='col-3 text-center p-2'>
-                                                {item.state === 0 ? <p>Result Awaited</p>
-                                                    : item.state === 1 ?
-                                                        <>
-                                                            <div className='d-flex justify-content-center'>
-                                                                <p>{item.maxWinning}</p>
-                                                            </div>
-                                                            <div className='d-flex justify-content-center'>
-                                                                <p>Win</p>
-                                                            </div>
-                                                        </>
-                                                        : <>
-                                                            <div className='d-flex justify-content-center'>
-                                                                <p>{item.maxWinning}</p>
-                                                            </div>
-                                                            <div className='d-flex justify-content-center'>
-                                                                <p>Lose</p>
-                                                            </div>
-                                                        </>
-                                                }
+                            {detailOpen === null ?
+                                <>
+                                    <div className='match-header'>
+                                        <div className='col-3 text-center'>Type</div>
+                                        <div className='col-4 text-center'>Date</div>
+                                        <div className='col-2 text-center'>Stake</div>
+                                        <div className='col-3 text-center'>Winnings</div>
+                                    </div>
+                                    <div className='match-body'>
+                                        {myBetData !== null && myBetData.length > 0 ?
+                                            myBetData.map((item, index) =>
+                                                <div key={index} onClick={() => setDetailOpen(index)} className={item.state === 0 ? 'bet-result-wait' : item.state === 1 ? 'bet-result-win' : 'bet-result-lose'}>
+                                                    <div className='col-3 text-center p-2'>{item.betSystem}</div>
+                                                    <div className='col-4 text-center p-2'>{GetTime(item.date)}</div>
+                                                    <div className='col-2 text-center p-2'>{item.initialStake.toFixed(2)}</div>
+                                                    <div className='col-3 text-center p-2'>
+                                                        {item.state === 0 ? <p>Result Awaited</p>
+                                                            : item.state === 1 ?
+                                                                <>
+                                                                    <div className='d-flex justify-content-center'>
+                                                                        <p>{item.maxWinning}</p>
+                                                                    </div>
+                                                                    <div className='d-flex justify-content-center'>
+                                                                        <p>Win</p>
+                                                                    </div>
+                                                                </>
+                                                                : <>
+                                                                    <div className='d-flex justify-content-center'>
+                                                                        <p>{item.maxWinning}</p>
+                                                                    </div>
+                                                                    <div className='d-flex justify-content-center'>
+                                                                        <p>Lose</p>
+                                                                    </div>
+                                                                </>
+                                                        }
+                                                    </div>
+                                                </div>
+                                            )
+                                            : <div> NO Data Found</div>}
+                                    </div>
+                                    <div className='match-footer text-center'></div>
+                                </>
+                            :
+                            <div className='p-3 pt-3'>
+                                <div className='d-flex justify-content-between'>
+                                    <div className='back-button' onClick={() => setDetailOpen(null)}>Back</div>
+                                    <div className='betslip-createbarcode-button'>Create Barcode</div>
+                                </div>
+                                <div className='d-flex status mt-3'>
+                                    <div className={myBetData[detailOpen].state === 0 ? 'result-awaited' : myBetData[detailOpen].state === 1 ? 'win' : 'lose'}>
+                                        {myBetData[detailOpen].state === 0 ? 'Result Awaited' : myBetData[detailOpen].state === 1 ? 'Win' : 'Lose'}
+                                    </div>
+                                </div>
+                                <div className='pt-2'>
+                                    <div className='pt-1 row row-data'>
+                                        <div className='yellow-text col-6'>Betting slip number:</div>
+                                        <div className='col-6'>{myBetData[detailOpen]._id}</div>
+                                    </div>
+                                    <div className='pt-1 row row-data'>
+                                        <div className='yellow-text col-6'>Date:</div>
+                                        <div className='col-6'>{GetTime(myBetData[detailOpen].date)}</div>
+                                    </div>
+                                    <div className='pt-1 row row-data'>
+                                        <div className='yellow-text col-6'>Number of bets:</div>
+                                        <div className='col-6'></div>
+                                    </div>
+                                    <div className='pt-1 row row-data'>
+                                        <div className='yellow-text col-6'>Stake per bet:</div>
+                                        <div className='col-6'>{myBetData[detailOpen].stakePerBet}</div>
+                                    </div>
+                                    <div className='pt-1 row row-data'>
+                                        <div className='yellow-text col-6'>Tax:</div>
+                                        <div className='col-6'>{myBetData[detailOpen].tax}</div>
+                                    </div>
+                                    <div className='pt-1 row row-data'>
+                                        <div className='yellow-text col-6'>Total stake:</div>
+                                        <div className='col-6'>{myBetData[detailOpen].initialStake}</div>
+                                    </div>
+                                    <div className='pt-1 row row-data'>
+                                        <div className='yellow-text col-6'>Payout:</div>
+                                        <div className='col-6'>{(myBetData[detailOpen].payout).toFixed(2)}</div>
+                                    </div>
+                                    <div className='pt-1 row row-data'>
+                                        <div className='yellow-text col-6'>Max winning:</div>
+                                        <div className='col-6'>{myBetData[detailOpen].winning}</div>
+                                    </div>
+                                </div>
+                                <div className='betslip-detail-rows mt-3'>
+                                    <div className='betslip-detail-row text-center'>Betting Events</div>
+                                    <div className='betslip-detail-row' onClick={() => funcHold(0)}>
+                                        <div className='d-flex pt-2'>
+                                            <div className='col-4'>Date</div>
+                                            <div className='col-5'>date</div>
+                                            <div className='bet-detail-icon bets-waiting col-3 d-flex justify-content-end'>
+                                                <p className={myBetData[detailOpen].state === 0 ? 'result-awaited' : myBetData[detailOpen].state === 1 ? 'win' : 'lose'}>
+                                                    <svg aria-hidden="true" data-fa-processed="" data-prefix="far" data-icon="arrow-alt-circle-down" className="svg-inline--fa fa-arrow-alt-circle-down fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm-32-316v116h-67c-10.7 0-16 12.9-8.5 20.5l99 99c4.7 4.7 12.3 4.7 17 0l99-99c7.6-7.6 2.2-20.5-8.5-20.5h-67V140c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12z"></path></svg>
+                                                </p>
                                             </div>
                                         </div>
-                                    )
-                                    : <div> NO Data Found</div>}
+                                        <div className='d-flex'>
+                                            <div className='col-4'>Betting Event</div>
+                                            <div className='col-8'>home team : away team</div>
+                                        </div>
+                                        <div className={hold === 0 ? '' : 'hold'}>
+                                            <div className='d-flex'>
+                                                <div className='col-4'>Score</div>
+                                                <div className='col-8'>xx</div>
+                                            </div>
+                                            <div className='d-flex'>
+                                                <div className='col-4'>Bank</div>
+                                                <div className='col-8'>xx</div>
+                                            </div>
+                                            <div className='d-flex'>
+                                                <div className='col-4'>Result</div>
+                                                <div className='col-8'>xx</div>
+                                            </div>
+                                            <div className='d-flex'>
+                                                <div className='col-4'>Tip</div>
+                                                <div className='col-8'>xx</div>
+                                            </div>
+                                            <div className='d-flex'>
+                                                <div className='col-4'>Odds</div>
+                                                <div className='col-8'>xx</div>
+                                            </div>
+                                            <div className='d-flex'>
+                                                <div className='col-4'>Winnings</div>
+                                                <div className='col-8'>xx</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='betslip-detail-row' onClick={() => funcHold(1)}>
+                                        <div className='d-flex pt-2'>
+                                            <div className='col-4'>Date</div>
+                                            <div className='col-5'>date</div>
+                                            <div className='bet-detail-icon bets-waiting col-3 d-flex justify-content-end'>
+                                                <p className={myBetData[detailOpen].state === 0 ? 'result-awaited' : myBetData[detailOpen].state === 1 ? 'win' : 'lose'}>
+                                                    <svg aria-hidden="true" data-fa-processed="" data-prefix="far" data-icon="arrow-alt-circle-down" className="svg-inline--fa fa-arrow-alt-circle-down fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm-32-316v116h-67c-10.7 0-16 12.9-8.5 20.5l99 99c4.7 4.7 12.3 4.7 17 0l99-99c7.6-7.6 2.2-20.5-8.5-20.5h-67V140c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12z"></path></svg>
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className='d-flex'>
+                                            <div className='col-4'>Betting Event</div>
+                                            <div className='col-8'>home team : away team</div>
+                                        </div>
+                                        <div className={hold === 1 ? '' : 'hold'}>
+                                            <div className='d-flex'>
+                                                <div className='col-4'>Score</div>
+                                                <div className='col-8'>xx</div>
+                                            </div>
+                                            <div className='d-flex'>
+                                                <div className='col-4'>Bank</div>
+                                                <div className='col-8'>xx</div>
+                                            </div>
+                                            <div className='d-flex'>
+                                                <div className='col-4'>Result</div>
+                                                <div className='col-8'>xx</div>
+                                            </div>
+                                            <div className='d-flex'>
+                                                <div className='col-4'>Tip</div>
+                                                <div className='col-8'>xx</div>
+                                            </div>
+                                            <div className='d-flex'>
+                                                <div className='col-4'>Odds</div>
+                                                <div className='col-8'>xx</div>
+                                            </div>
+                                            <div className='d-flex'>
+                                                <div className='col-4'>Winnings</div>
+                                                <div className={myBetData[detailOpen].state === 0 ? 'result-awaited col-8 status' : myBetData[detailOpen].state === 1 ? 'win col-8 status' : 'lose col-8 status'}></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div className='match-footer text-center'></div>
-                        </div>
-                        :
-                        <div className='myaccount-table'>
-                            <div></div>
-                        </div>
-                    }
+                        }
+                    </div>
                 </div>
             </div>
             <MobileFooter />
